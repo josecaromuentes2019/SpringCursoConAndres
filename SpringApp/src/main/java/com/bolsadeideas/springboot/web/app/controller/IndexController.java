5bc00a83525088f26389e57d019059702b8a87bc
Jose Eusebio Caro Muentes <jcaro@heinsohn.com.co>
2022-08-08 23:20:47 -0500

    fin seccion 79 curso de sprig con Andres

--- SpringApp/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ SpringApp/src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,18 @@ import java.util.Map;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-    @GetMapping({"/index",""})
+
+    @Value("${texto.indexController.index.titulo}")
+    private String indexTitulo;
+
+    @Value("${texto.indexController.listar.titulo}")
+    private  String indexPerfil;
+
+    @Value("${texto.indexController.listar.titulo}")
+    private String indexListar;
+    @GetMapping({"/index","","/"})
     public String index(Map<String , Object> map){
-        map.put("titulo","Hola Mundo con mapa");
+        map.put("titulo",indexTitulo);
         return "index";
     }
 
@@ -30,6 +40,7 @@ public class IndexController {
         usuario.setEmail("jose@correo");
 
         modelo.addAttribute("usuario",usuario);
+        modelo.addAttribute("titulo",indexPerfil);
         return  "perfil";
     }
 
@@ -37,15 +48,15 @@ public class IndexController {
     public String listar(Model modelo){
         Usuario usuario = new Usuario();
 
-        List<Usuario> usuarios = Arrays.asList(
+       /* List<Usuario> usuarios = Arrays.asList(
                 new Usuario("Jose","Caro","muentes@correo"),
                 new Usuario("Juan","Perez","juan@correo"),
                 new Usuario("Luisa","Lopez","lopez@correo"),
                 new Usuario("Tom","Matias","tom@correo")
-        );
+        );*/
 
         /*modelo.addAttribute("usuarios",usuarios);*/
-        modelo.addAttribute("titulo","Listar usuarios");
+        modelo.addAttribute("titulo",indexListar);
         return "listar";
     }
 
